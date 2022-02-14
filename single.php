532576707f7a9a97b7383eb72933bb968dc89b31
bnbabu55 <bnbabu@gmail.com>
2022-02-14 21:57:37 +0530

    style fix

--- single.php
+++ single.php
@@ -17,17 +17,17 @@ global $wp_query;
 while (have_posts()) :
 	the_post();
 ?>
-	<div class="py-16 w-full px-5 lg:px-0 lg:w-10/12 lg:mx-auto ">
+	<div class="w-full px-5 py-16 lg:px-0 lg:w-10/12 lg:mx-auto ">
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<header class="py-3 -mt-2 text-5xl leading-none tracking-wide text-left font-PTSerif text-themeAccent pb-5">
+			<header class="py-3 pb-5 -mt-2 text-5xl leading-none tracking-wide text-left font-PTSerif text-themeAccent">
 				<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
 
 			</header><!-- .entry-header -->
-			<h2 class="text-2xl pb-5 font-bold">
+			<h2 class="pb-5 text-2xl font-bold">
 				<?php echo get_the_date('F j, Y'); ?>
 			</h2>
-			<div class="prose-lg lg:prose-xl">
+			<div class="prose-lg lg:prose-xl post-body">
 				<?php
 				the_content();
 
@@ -39,7 +39,7 @@ while (have_posts()) :
 <?php
 endwhile; // End of the loop.
 ?>
-<div class="post-navigation py-8 w-full px-5 lg:px-0 lg:w-10/12 lg:mx-auto flex flex-col lg:flex-row justify-between">
+<div class="flex flex-col justify-between w-full px-5 py-8 post-navigation lg:px-0 lg:w-10/12 lg:mx-auto lg:flex-row">
 	<div><?php previous_post_link('%link', '<< Previous post', false, ''); ?></div>
 	<div><?php next_post_link('%link', 'Next post >>', false, ''); ?></div>
 </div>
